d4406b506320cb76f35fafef364d3dea42793ee2
burger2020 <blac2013@gmail.com>
2023-06-29 12:37:43 +0900

    first

---
+++ src/test/kotlin/com/prac/msa/awsmsauserservice/awsmsauserservice/AwsMsaUserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.prac.msa.awsmsauserservice.awsmsauserservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsMsaUserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
